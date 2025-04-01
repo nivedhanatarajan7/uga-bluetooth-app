e992f6f8f5a5d19e9c0bfb5b108abd2bcf16b14f
Lincoln Ledet <lincolnledet@gmail.com>
2025-04-01 18:15:25 -0400

    Real time temp and humidity

--- uga-bluetooth-app/app/(tabs)/index.tsx
+++ uga-bluetooth-app/app/(tabs)/index.tsx
@@ -3,14 +3,18 @@ import { View, Text, FlatList, TouchableOpacity, PermissionsAndroid, Platform, B
 import { BleManager, Device } from 'react-native-ble-plx';
 import base64 from 'react-native-base64';
 import { Float } from 'react-native/Libraries/Types/CodegenTypes';
+import { get } from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
 
 const manager = new BleManager();
 const SERVICE_UUID = "12630000-cc25-497d-9854-9b6c02c77054";
 const TEMP_CHARACTERISTIC_UUID = "12630001-cc25-497d-9854-9b6c02c77054"; 
+const HUMID_CHARACTERISTIC_UUID = "12630002-cc25-497d-9854-9b6c02c77054"; 
 export default function App() {
   const [devices, setDevices] = useState<Device[]>([]);
   const [device, setDevice] = useState<Device | null>(null);
   const [temperature, setTemperature] = useState<number | null>(null);
+  const [humidity, setHumidity] = useState<number | null>(null);
+
   const [scanning, setScanning] = useState(false);
   let tempN = false;
 
@@ -65,15 +69,37 @@ export default function App() {
     }
   };
 
+  const getHumidity = async (device: Device) => {
+    try {
+        // so
+        const humidityCharacteristic = await device.readCharacteristicForService(SERVICE_UUID, HUMID_CHARACTERISTIC_UUID); //WTF DOES THIS DO // oh shit i figured it out. 
+        console.log(humidityCharacteristic); // Let me see them BYTES!
+
+        if (humidityCharacteristic?.value) {
+          // Converts base 64 byte array into byte array
+          // atob converts a string of Base64 characters to bytes, converts each element to decimal value and makes it into an array of 4 ints
+          const humval = Uint8Array.from(atob(humidityCharacteristic.value), c => c.charCodeAt(0));
+          const humidityval = ((humval[2] << 8) | humval[1]) / 100;
+          console.log("humidity:", humidityval);
+          
+            setHumidity(humidityval);
+            tempN = true;
+        }
+    } catch (error) {
+        console.error("Error reading Humidity:", error);
+    }
+
+};
+
 const getTemperature = async (device: Device) => {
         try {
-            const tempCharacteristic = await device.readCharacteristicForService(SERVICE_UUID, TEMP_CHARACTERISTIC_UUID);
+            const tempCharacteristic = await device.readCharacteristicForService(SERVICE_UUID, TEMP_CHARACTERISTIC_UUID); //WTF DOES THIS DO 
+            console.log(tempCharacteristic); // Let me see them BYTES!
 
             if (tempCharacteristic?.value) {
               // Converts base 64 byte array into byte array
               // atob converts a string of Base64 characters to bytes, converts each element to decimal value and makes it into an array of 4 ints
               const tempval = Uint8Array.from(atob(tempCharacteristic.value), c => c.charCodeAt(0));
-
               const temperatureval = ((tempval[2] << 8) | tempval[1]) / 100;
               console.log("Temperature:", temperatureval);
               
@@ -92,6 +118,7 @@ useEffect(() => {
   if (device) {
     interval = setInterval(() => {
       getTemperature(device);
+      getHumidity(device);
     }, 1000);
   }
 
@@ -126,6 +153,7 @@ useEffect(() => {
 
       {device && <Text>Connected to: {device.name}</Text>}
       {temperature !== null && <Text>Temperature: {temperature}°C</Text>}
+      {humidity !== null && <Text>Humidity: {humidity}%</Text>}
     </View>
   );
 }
